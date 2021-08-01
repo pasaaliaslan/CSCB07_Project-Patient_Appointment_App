1540a7d673b6504e4abb0637f31ea53b0b4fd5f7
Pasa <pasaali1603@gmail.com>
2021-08-01 21:17:29 +0300

    Firebase Example Try 1

--- app/src/main/java/com/example/cscb07project_patientappointmentapp/MainActivity.java
+++ app/src/main/java/com/example/cscb07project_patientappointmentapp/MainActivity.java
@@ -3,15 +3,29 @@ package com.example.cscb07project_patientappointmentapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    // HELLO WORLD
-    // Added Something
-    // Added Something Else
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference();
+
+        Patient p1 = new Patient(123456789, "Pasa");
+
+        ref.child("Patients").child(p1.toString()).setValue(p1);
+
     }
 }
---
+++ app/src/main/java/com/example/cscb07project_patientappointmentapp/Patient.java
@@ -0,0 +1,25 @@
+package com.example.cscb07project_patientappointmentapp;
+
+public class Patient {
+    long id;
+    String name;
+
+    public Patient(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(id);
+    }
+
+}
