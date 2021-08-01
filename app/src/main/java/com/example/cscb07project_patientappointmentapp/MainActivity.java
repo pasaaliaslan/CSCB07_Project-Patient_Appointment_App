096c5820837ca8894fd995b67051fa890487ab18
Pasa <pasaali1603@gmail.com>
2021-08-01 17:08:32 +0300

    InitialCommit

--- app/src/main/java/com/example/cscb07project_patientappointmentapp/MainActivity.java
+++ app/src/main/java/com/example/cscb07project_patientappointmentapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // HELLO WORLD ????
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
