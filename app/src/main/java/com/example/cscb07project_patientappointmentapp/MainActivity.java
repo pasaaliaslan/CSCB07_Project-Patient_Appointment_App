2115238d8c6e8292230c099ebf98de4e9b912f26
Pasa <pasaali1603@gmail.com>
2021-08-02 22:59:26 +0300

    Firebase Example Try 1

--- app/src/main/java/com/example/cscb07project_patientappointmentapp/MainActivity.java
+++ app/src/main/java/com/example/cscb07project_patientappointmentapp/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        // HIHIHIH
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
