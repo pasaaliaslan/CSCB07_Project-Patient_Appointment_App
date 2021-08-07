3688ad194032b5bcac064dcd8413fa32c8d594bf
HaHammad <79287888+HaHammad@users.noreply.github.com>
2021-08-07 14:42:42 -0400

    got getDate and getTime and added remaining getters and setters for PersonClass

--- app/src/main/java/com/example/cscb07project_patientappointmentapp/Objects/Appointment.java
+++ app/src/main/java/com/example/cscb07project_patientappointmentapp/Objects/Appointment.java
@@ -6,9 +6,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
-
+import java.util.Arrays;
 import com.example.cscb07project_patientappointmentapp.Objects.Doctor;
 import com.example.cscb07project_patientappointmentapp.Objects.Patient;
 
@@ -28,7 +29,15 @@ public class Appointment{
         this.id = id;
     }
 
-//    public String GetDate(){
-//        return(this.startTime.toDate())
-//    }
+    public String getDate(){
+       String hold = this.startTime.toDate().toString();
+       List<String> splits = Arrays.asList(hold.split("\\s+"));
+       return(splits.get(0) + ", " + splits.get(1) + " " + splits.get(2) + ", " +splits.get(5));
+    }
+
+    public String getTime(){
+        String hold = this.startTime.toDate().toString();
+        List<String> splits = Arrays.asList(hold.split("\\s+"));
+        return(splits.get(3));
+    }
 }
