45b5949393c45a57cf45a8b0356c9175507f5b18
alinabuzila <88336627+alinabuzila@users.noreply.github.com>
2021-08-08 13:02:44 -0400

    Commit 2

--- app/src/main/java/com/example/cscb07project_patientappointmentapp/Person.java
+++ app/src/main/java/com/example/cscb07project_patientappointmentapp/Person.java
@@ -1,8 +1,6 @@
 package com.example.cscb07project_patientappointmentapp;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Locale;
 
 public abstract class Person {
     protected enum Gender{
@@ -13,14 +11,30 @@ public abstract class Person {
     String fullName;
     String username;
     String password;
+    UID uid;
     Gender gender;
+    ArrayList<UID> upcomingAppointments;
+    ArrayList<UID> pastAppointments;
 
-    public Person(String fullName, String username, String password, String gender){
+    public Person(String fullName, String username, String password, String gender, UID uid){
         this.fullName = fullName;
         this.username = username;
         this.password = password;
+        this.uid = uid;
         this.gender = Gender.valueOf(gender.toUpperCase());
+        this.upcomingAppointments = new ArrayList<UID>();
+        this.pastAppointments = new ArrayList<UID>();
     }
+
+    public UID getUID() { return uid; }
+
+    public void setUID(UID uid) { this.uid = uid;}
+
+    public ArrayList<UID> getAppointments() {
+        return upcomingAppointments;
+    }
+
+    public void setAppointments(ArrayList<UID> appointments) { this.upcomingAppointments = appointments; }
 
     public String getFullName() {
         return fullName;
@@ -49,5 +63,5 @@ public abstract class Person {
     public void setGender(Gender gender) {
         this.gender = gender;
     }
-
 }
+
