30562ba852ea3b5cf95c812967756b3bd9895de9
alinabuzila <88336627+alinabuzila@users.noreply.github.com>
2021-08-09 13:17:19 -0400

    Adding DoctorIDtoDoctorAdapter to alinas UI work

--- app/src/main/java/com/example/cscb07project_patientappointmentapp/DoctorIDtoDoctorAdapter.java
+++ app/src/main/java/com/example/cscb07project_patientappointmentapp/DoctorIDtoDoctorAdapter.java
@@ -1,17 +1,4 @@
 package com.example.cscb07project_patientappointmentapp;
 
-//import com.example.cscb07project_patientappointmentapp.Objects.*;
-
-import com.google.firebase.Timestamp;
-
 public class DoctorIDtoDoctorAdapter {
-    // Create a function that returns doctor object corresponding to the doctor ID
-
-//    public static Doctor getDoctor(String doctorID){
-//        // Search doctor in the database here and act accordingly
-//
-//        // Doctor doctor = new Doctor("asd", "asd","asd","MALE","IMMUNOLOGY", startTime, endTime);
-//
-//        return doctor;
-//    }
 }
